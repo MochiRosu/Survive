cfd6612fd68fa81f81292191b53a9bae40d824b8
Roscoe Collard <129026799+MochiRosu@users.noreply.github.com>
2023-12-03 01:09:48 +1100

    Added the player and the movement script

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform CameraPosition;
+    private void Update()
+    {
+        transform.position = CameraPosition.position;
+    }
+}
