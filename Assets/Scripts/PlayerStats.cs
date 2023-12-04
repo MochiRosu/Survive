2d3c73086d6ab8687bfd191620b1ab0df11d2c0b
BunBaoi <135566105+BunBaoi@users.noreply.github.com>
2023-12-05 04:45:40 +1100

    Player Stats
    Food & Water subtract every few seconds
    If statements when Food || Water = 0
    Subtract Health -1
    WORKS

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,32 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
+    [Header("Player Stats")]
     [SerializeField] int HealthBar = 100;
     [SerializeField] int WaterBar = 100;
     [SerializeField] int FoodBar = 100;
-    int num = 1;
 
-    private void Start()
+    [Header("Deduction")]
+    int StatsDeduction = 1;
+    int TimerInterval = 3;
+
+    float timer = 0f;
+
+    [Header("Text Box")]
+    public Text Health;
+    public Text Food;
+    public Text Water;
+
+    void Update()
     {
+        Health.text = "Health: " + HealthBar;
+        Food.text = "Food: " + FoodBar;
+        Water.text = "Water: " + WaterBar;
 
+        timer += Time.deltaTime;
+
+        DeductNumber();
+        Debug.Log(HealthBar);
     }
 
-    private void Update()
+    void DeductNumber()
     {
-        WaterBar -= num;
+        if (timer >= TimerInterval)
+        {
+            timer = 0;
+            Debug.Log(FoodBar + " and " + WaterBar);
+
+            FoodAndWaterDeduction();
+        }
+    }
+
+    void FoodAndWaterDeduction()
+    {
+        // Deduct the specified amount from food and water
+        FoodBar -= StatsDeduction;
+        WaterBar -= StatsDeduction;
+
+        // Ensure food and water don't go below zero
+        FoodBar = Mathf.Max(FoodBar, 0);
+        WaterBar = Mathf.Max(WaterBar, 0);
+
+        HealthCheck();
     }
 
     void HealthCheck()
     {
-        if (FoodBar <= num)
+        if (FoodBar <= 0 || WaterBar <= 0)
         {
-            if (WaterBar <= num)
-            {
-                HealthBar -= 1;
-            }
+         HealthBar -= 1;
         }
     }
 
