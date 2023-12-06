8a095ee6795cb2cab00ab09b87f2fac406871182
Roscoe Collard <129026799+MochiRosu@users.noreply.github.com>
2023-12-07 01:41:34 +1100

    added a terrain maker

---
+++ Assets/Scripts/Wood.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wood", menuName = "ScriptableObjects/Wood")]
+
+public class Wood : ScriptableObject
+{
+    public string myString;
+}
